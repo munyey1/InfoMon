851715cabea8642419c551f7f29290d1cefe8e63
Michael Ly <michaelly60@outlook.com>
2025-01-02 01:25:14 +0000

    add test component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-
+import Test from "./components/Test.vue";
 </script>
 
 <template>
-
+  <main>
+    <Test />
+  </main>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
