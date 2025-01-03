a4ea1e636d6c353e76e0b5444c10a2e91d6e01e8
Michael Ly <64155853+munyey1@users.noreply.github.com>
2025-01-03 20:10:42 +0000

    testing

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,36 @@
 <script setup>
+import { ref, onMounted } from "vue";
+
 import Test from "./components/Test.vue";
+
+const isDarkMode = ref(true);
+
+onMounted(() => {
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === "dark") {
+    isDarkMode.value = true;
+  }
+});
+
+const toggleDarkMode = () => {
+  isDarkMode.value = !isDarkMode.value;
+
+  localStorage.setItem("theme", isDarkMode.value ? "dark" : "light");
+};
 </script>
 
 <template>
+  <div>
+    <button
+      @click="toggleDarkMode"
+      class="mt-6 bg-indigo-500 text-white px-4 py-2 shadow-lg"
+    >
+      {{ isDarkMode ? "Light Mode" : "Dark Mode" }}
+    </button>
+  </div>
   <main>
-    <Test />
+    <Test :isDarkMode="isDarkMode" />
   </main>
 </template>
 
