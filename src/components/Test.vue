b0dc09bb31482b6a1a07e23b3ace2bdd5c51fb5a
Michael Ly <64155853+munyey1@users.noreply.github.com>
2025-01-09 20:46:45 +0000

    PokeAPI call, select dropdown
    Added a PokeAPI call that gets the og 151 mons. Need to give it a parameter for it to get the correct gen mons based on the select dropdown.

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,10 +1,33 @@
 <script setup>
 import { ref, onMounted } from "vue";
+import axios from "axios";
+
+const allPokemon = ref([]);
+const loading = ref(true);
+const error = ref(null);
+
+const selectedGen = ref("");
+
+const fetchPokemon = async () => {
+  const url = "https://pokeapi.co/api/v2/pokemon?limit=151";
+
+  try {
+    const response = await axios.get(url);
+    allPokemon.value = response.data.results;
+  } catch (err) {
+    error.value = err;
+  } finally {
+    loading.value = false;
+  }
+};
 
 const props = defineProps({
   isDarkMode: Boolean,
 });
 
+const get151Pokemon = () => {
+  fetchPokemon();
+};
 </script>
 
 <template>
@@ -15,12 +38,39 @@ const props = defineProps({
       <h3
         class="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight"
       >
-        Writes Upside-Down
+        Pokemon
       </h3>
-      <p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
-        The Zero Gravity Pen can be used to write in any orientation, including
-        upside-down. It even works in outer space.
-      </p>
+      <div>
+        <select class="select bg-white dark:bg-slate-800 w-full max-w-xs" v-model="selectedGen">
+          <option value="" selected disabled>Please select</option>
+          <option value="1">Generation 1</option>
+          <option value="2">Generation 2</option>
+          <option value="3">Generation 3</option>
+          <option value="4">Generation 4</option>
+          <option value="5">Generation 5</option>
+          <option value="6">Generation 6</option>
+          <option value="7">Generation 7</option>
+          <option value="8">Generation 8</option>
+        </select>
+      </div>
+      <button
+        @click="getPokemon"
+        class="btn mt-6 bg-indigo-500 text-white px-4 py-2 shadow-lg"
+      >
+        Get 151 Pokemon
+      </button>
+      <div v-if="error" class="mt-4">
+        <p class="text-slate-500 dark:text-slate-400 text-sm">
+          {{ error.message }}
+        </p>
+      </div>
+      <div v-else class="mt-4">
+        <ul>
+          <li v-for="pokemon in allPokemon" :key="pokemon.name">
+            {{ pokemon.name }}
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
