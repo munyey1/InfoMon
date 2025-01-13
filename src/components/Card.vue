e78eb3eeb40083192590eee712cdf4ad18453136
Michael Ly <64155853+munyey1@users.noreply.github.com>
2025-01-13 20:48:42 +0000

    New files for the card and search component

---
+++ src/components/Card.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+import { defineProps } from "vue";
+
+const props = defineProps({
+  isDarkMode: Boolean,
+});
+
+</script>
+
+<template>
+  <div :class="isDarkMode ? 'dark-mode' : ''">
+    <div class="card bg-white dark:bg-slate-800 shadow-xl">
+      <figure>
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+          alt="Shoes"
+        />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">
+          Shoes!
+          <div class="badge badge-secondary">NEW</div>
+        </h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+          <div class="badge badge-outline">Fashion</div>
+          <div class="badge badge-outline">Products</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Search.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+import Card from "./Card.vue";
+
+</script>
+
+<template>
+  <div>
+    <Card />
+  </div>
+</template>
+
+<style scoped></style>
