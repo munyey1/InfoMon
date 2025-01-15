b12e8d0751c8707e5ff9abe4cd29513e14f11ada
Michael Ly <64155853+munyey1@users.noreply.github.com>
2025-01-15 20:02:13 +0000

    Create TypeTag.vue

---
+++ src/components/TypeTag.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { defineProps } from "vue";
+
+import { typeColorMap } from "@/utils/typeColourMap";
+
+const props = defineProps({
+  type: String,
+});
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped></style>
