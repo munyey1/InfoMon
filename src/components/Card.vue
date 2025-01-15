f515956bff31c6f3b9a95e87ff993703d9d00806
Michael Ly <64155853+munyey1@users.noreply.github.com>
2025-01-15 20:35:19 +0000

    dynamic tags, testing props

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,19 +3,20 @@ import { ref, onMounted } from "vue";
 import axios from "axios";
 import { defineProps } from "vue";
 
+import { typeColorMap } from "@/utils/typeColourMap";
+
 const props = defineProps({
   isDarkMode: Boolean,
+  name: String,
   url: String,
 });
 
-const pokemon = ref(null);
-const spriteUrl = ref([]);
 const loading = ref(true);
 const error = ref(null);
 
+const pokemon = ref(null);
+const spriteUrl = ref([]);
 const types = ref([]);
-
-const tt = ref("green-500");
 
 const fetchPokemon = async () => {
   const genUrl = `https://pokeapi.co/api/v2/pokemon/1/`;
@@ -24,6 +25,9 @@ const fetchPokemon = async () => {
     const response = await axios.get(genUrl);
     pokemon.value = response.data;
     spriteUrl.value = response.data.sprites.other["official-artwork"];
+
+    types.value = pokemon.value.types.map((type) => type.type.name);
+
   } catch (err) {
     error.value = err;
   } finally {
@@ -36,10 +40,9 @@ onMounted(() => {
 });
 
 const testBtn = () => {
-  types.value = pokemon.value.types.map((type) => type.type.name);
   console.log(types.value[0]);
+  console.log(typeColorMap[types.value[0]]);
 };
-
 </script>
 
 <template>
@@ -52,15 +55,21 @@ const testBtn = () => {
           :key="sprites"
           class="grid grid-rows-1 gap-4"
         >
-          <img :src="sprites" class="h-auto w-fit p-4"/>
+          <img :src="sprites" class="h-auto w-fit p-4" />
         </div>
       </figure>
       <div class="card-body">
-        <h2 class="card-title">Bulbasaur
-          <div class="badge text-white" :class="`bg-${tt}`">Grass</div>
-          <div class="badge bg-fuchsia-500 text-white">Poison</div>
-        </h2> 
-        <p>Check out Bulbasaur's official artwork!</p>
+        <h2 class="card-title capitalize">
+          {{ props.name }}
+          <div
+            v-for="type in types"
+            :key="type"
+            class="badge text-white capitalize"
+            :class="typeColorMap[type] || 'bg-gray-500'"
+          >
+            {{ type }}
+          </div>
+        </h2>
       </div>
     </div>
   </div>
