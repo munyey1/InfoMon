7505d16e0137fa9723201fde323572230a406133
Michael Ly <64155853+munyey1@users.noreply.github.com>
2025-01-25 17:35:37 +0000

    use pixel sprites and remove extra info

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -27,7 +27,7 @@ const fetchPokemon = async () => {
   try {
     const response = await axios.get(genUrl);
     pokemon.value = response.data;
-    spriteUrl.value = response.data.sprites.other["official-artwork"];
+    spriteUrl.value = response.data.sprites;
 
     types.value = pokemon.value.types.map((type) => type.type.name);
   } catch (err) {
@@ -54,61 +54,35 @@ const testBtn = () => {
 <template>
   <div :class="isDarkMode ? 'dark-mode' : ''">
     <div class="card bg-white dark:bg-slate-800 shadow-xl">
-      <figure>
-        <div class="grid grid-cols-1 justify-items-center">
-          <button
-            @click="toggleShiny"
-            class="btn bg-indigo-500 text-white mt-8"
-          >
-            Toggle Sprite
-          </button>
-          <p class="mt-4 text-slate-900 dark:text-slate-400 text-lg font-semibold">
-            {{ showShiny ? "Shiny" : "Default" }}
-          </p>
-          <img
-            v-if="showShiny"
-            :src="spriteUrl.front_shiny"
-            class="h-auto w-full p-4"
-          />
-          <img
-            v-else
-            :src="spriteUrl.front_default"
-            class="h-auto w-full p-4"
-          />
-        </div>
-      </figure>
       <div class="card-body">
-        <h2 class="card-title text-slate-900 dark:text-slate-400 capitalize text-2xl">
+        <h2 class="card-title text-slate-900 dark:text-slate-400 capitalize">
           {{ props.name }}
-          <div
-            v-for="type in types"
-            :key="type"
-            class="badge text-white capitalize"
-            :class="typeColorMap[type] || 'bg-gray-500'"
-          >
-            {{ type }}
-          </div>
         </h2>
-        <div v-if="!loading" class="text-base text-slate-900 dark:text-slate-400">
-          <p>
-            National Number: {{ pokemon.id }}
-            <br />
-            Height: {{ pokemon.height / 10 }}m
-            <br />
-            Weight: {{ pokemon.weight / 10 }}kg
-          </p>
-         <p class="mt-4 text-lg font-semibold" v-if="pokemon.abilities.length > 0">
-            Abilities:
-         </p>
-          <ul>
-            <li v-for="ability in pokemon.abilities" :key="ability.ability.name">
-              <ToolTip :url="ability.ability.url" />
-            </li>
-          </ul>
+        <figure>
+          <div class="">
+            <img
+              v-if="showShiny"
+              :src="spriteUrl.front_shiny"
+              class="h-auto w-fit"
+            />
+            <img
+              v-else
+              :src="spriteUrl.front_default"
+              class="h-auto w-full"
+            />
+          </div>
+        </figure>
+        <div
+          v-for="type in types"
+          :key="type"
+          class="badge text-white capitalize font-semibold"
+          :class="typeColorMap[type] || 'bg-gray-500'"
+        >
+          {{ type }}
         </div>
-        <button @click="testBtn" class="btn bg-indigo-500 text-white mt-4">
-          Test
-        </button>
+        <div v-if="!loading" class="text-base font-semibold text-slate-900 dark:text-slate-400">
+            #{{ pokemon.id }}
+        </div>
       </div>
     </div>
   </div>
