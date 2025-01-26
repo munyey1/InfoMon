d5d860dd0a4fe5bb2e878122a3e114d8471c8963
Michael Ly <64155853+munyey1@users.noreply.github.com>
2025-01-26 19:38:18 +0000

    add name, sprite, no_, height and weight

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -11,10 +11,13 @@ const pokemon = ref(null);
 const loading = ref(true);
 const error = ref(null);
 
+const imgUrl = ref("");
+
 const fetchPokemon = async () => {
   try {
     const response = await axios.get(props.url);
     pokemon.value = response.data;
+    imgUrl.value = response.data.sprites.other["official-artwork"].front_default;
   } catch (err) {
     error.value = err;
   } finally {
@@ -28,11 +31,29 @@ onMounted(() => {
 </script>
 
 <template>
-  <div v-if="error">
-    {{ error.message }}
-  </div>
-  <div v-if="pokemon">
-    {{ pokemon.name }}
+  <div :class="isDarkMode ? 'dark-mode' : ''" class="border-2 w-full">
+    <div v-if="error">
+      {{ error.message }}
+    </div>
+    <div v-if="pokemon" class="grid grid-cols-2 justify-items-center">
+      <h1 class="text-2xl font-bold col-span-2">
+        {{ pokemon.name }}
+      </h1>
+      <figure class="col-span-2">
+        <img :src="imgUrl" />
+      </figure>
+      <div class="text-base">
+        <p>
+          Pokedex No: <span class="font-bold">#{{ pokemon.id }}</span>
+        </p>
+        <p>
+          Height: <span class="font-bold">{{ pokemon.height/10 }}m</span>
+        </p>
+        <p>
+          Weight: <span class="font-bold">{{ pokemon.weight/10 }}kg</span>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
